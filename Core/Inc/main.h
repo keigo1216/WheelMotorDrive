e76c538be4a06629266491c1eb9ab2b9ff663d4c
shibatakeigo <sagankeigo17@gmail.com>
2025-04-11 14:04:43 +0900

    Added read encoder code

---
+++ Core/Inc/main.h
@@ -0,0 +1,95 @@
+/* USER CODE BEGIN Header */
+/**
+  ******************************************************************************
+  * @file           : main.h
+  * @brief          : Header for main.c file.
+  *                   This file contains the common defines of the application.
+  ******************************************************************************
+  * @attention
+  *
+  * Copyright (c) 2025 STMicroelectronics.
+  * All rights reserved.
+  *
+  * This software is licensed under terms that can be found in the LICENSE file
+  * in the root directory of this software component.
+  * If no LICENSE file comes with this software, it is provided AS-IS.
+  *
+  ******************************************************************************
+  */
+/* USER CODE END Header */
+
+/* Define to prevent recursive inclusion -------------------------------------*/
+#ifndef __MAIN_H
+#define __MAIN_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/* Includes ------------------------------------------------------------------*/
+#include "stm32g4xx_hal.h"
+
+/* Private includes ----------------------------------------------------------*/
+/* USER CODE BEGIN Includes */
+
+/* USER CODE END Includes */
+
+/* Exported types ------------------------------------------------------------*/
+/* USER CODE BEGIN ET */
+
+/* USER CODE END ET */
+
+/* Exported constants --------------------------------------------------------*/
+/* USER CODE BEGIN EC */
+
+/* USER CODE END EC */
+
+/* Exported macro ------------------------------------------------------------*/
+/* USER CODE BEGIN EM */
+
+/* USER CODE END EM */
+
+void HAL_TIM_MspPostInit(TIM_HandleTypeDef *htim);
+
+/* Exported functions prototypes ---------------------------------------------*/
+void Error_Handler(void);
+
+/* USER CODE BEGIN EFP */
+
+/* USER CODE END EFP */
+
+/* Private defines -----------------------------------------------------------*/
+#define ENCODER_A_1_Pin GPIO_PIN_0
+#define ENCODER_A_1_GPIO_Port GPIOA
+#define ENCODER_B_1_Pin GPIO_PIN_1
+#define ENCODER_B_1_GPIO_Port GPIOA
+#define ENCODER_A_2_Pin GPIO_PIN_4
+#define ENCODER_A_2_GPIO_Port GPIOA
+#define ENCODER_B_2_Pin GPIO_PIN_6
+#define ENCODER_B_2_GPIO_Port GPIOA
+#define PWM_REVERSE_3_Pin GPIO_PIN_7
+#define PWM_REVERSE_3_GPIO_Port GPIOA
+#define PWM_FORWARD_1_Pin GPIO_PIN_8
+#define PWM_FORWARD_1_GPIO_Port GPIOA
+#define PWM_REVERSE_1_Pin GPIO_PIN_9
+#define PWM_REVERSE_1_GPIO_Port GPIOA
+#define PWM_FORWARD_2_Pin GPIO_PIN_10
+#define PWM_FORWARD_2_GPIO_Port GPIOA
+#define PWM_REVERSE_2_Pin GPIO_PIN_11
+#define PWM_REVERSE_2_GPIO_Port GPIOA
+#define PWM_FORWARD_3_Pin GPIO_PIN_12
+#define PWM_FORWARD_3_GPIO_Port GPIOA
+#define ENCODER_A_3_Pin GPIO_PIN_6
+#define ENCODER_A_3_GPIO_Port GPIOB
+#define ENCODER_B_3_Pin GPIO_PIN_7
+#define ENCODER_B_3_GPIO_Port GPIOB
+
+/* USER CODE BEGIN Private defines */
+
+/* USER CODE END Private defines */
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* __MAIN_H */
